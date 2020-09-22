b9b5d714b4f2a9dfd01f116ef3adab322cdf093f
Andrew M. Pressel <ufotofu@whistlinglemons.com>
2020-09-22 17:42:56 -0400

    perltidy; library support

--- src/bin/git.sync.pl
+++ src/bin/git.sync.pl
@@ -7,11 +7,10 @@ use autodie;
 use Getopt::Long;
 use YAML::Tiny;
 
-
 use Toolkit;
 
 my %opts;
-if ( !GetOptions( \%opts, 'verbose','commit' ) ) {
+if ( !GetOptions( \%opts, 'verbose', 'commit' ) ) {
   die("Invalid incantation\n");
 }
 
@@ -21,26 +20,57 @@ exit(0);
 sub main {
 
   my $rcFile = "$ENV{'HOME'}/.gitsyncrc";
-  if( !-f $rcFile ){
+  if ( !-f $rcFile ) {
     local $/ = undef;
     my @config = <DATA>;
 
-    open(my $fh, '>', $rcFile);
-    $fh->print(join('', @config),"\n");
+    open( my $fh, '>', $rcFile );
+    $fh->print( join( '', @config ), "\n" );
     close($fh);
   }
-  
+
   my $config = YAML::Tiny->read($rcFile);
   $config = $config->[0];
 
-  printObject($config) if( $opts{'verbose'});
+  printObject($config) if ( $opts{'verbose'} );
 
-  foreach my $key ( sort keys %{$config} ) {
-    my @changed = createFake( $config->{$key} );
+  my $binaries = $config->{'binaries'};
+  foreach my $key ( sort keys %{$binaries} ) {
+    my @changed = createFake( $binaries->{$key} );
     if ( scalar @changed ) {
-      message( $key => map{ green("  $_") } @changed );
+      message( $key => map { green("  $_") } @changed );
     }
   }
+
+  my $libs = $config->{'libraries'};
+  foreach my $key ( sort keys %{$libs} ) {
+    my @changed = checkPerlToolkit( $libs->{$key} );
+    if ( scalar @changed ) {
+      message( $key => map { green("  $_") } @changed );
+    }
+  }
+}
+
+sub checkPerlToolkit {
+  my ($repo) = @_;
+
+  my @retval;
+  chdir($repo);
+  foreach my $subdir (qw{ bin lib }) {
+    if ( -d $subdir ) {
+      my @files = glob("$subdir/*");
+      foreach my $file ( sort @files ) {
+        if ( system("diff -q $file $ENV{'HOME'}/$file > /dev/null 2>&1") ) {
+          push( @retval, $file);
+        } else {
+          quick( same => $file ) if ( $opts{'verbose'} );
+        }
+      }
+
+    }
+  }
+
+  return wantarray ? @retval : \@retval;
 }
 
 sub createFake {
@@ -59,14 +89,12 @@ sub createFake {
   foreach my $ln (@output) {
     chomp($ln);
 
-    if( $ln =~ /^rsync/ ){
-      my @chunk = split(/\s+/, $ln);
+    if ( $ln =~ /^rsync/ ) {
+      my @chunk = split( /\s+/, $ln );
       $dir = $chunk[-2];
     }
 
-
-
-    quick($ln) if( $opts{'verbose'} );
+    quick($ln) if ( $opts{'verbose'} );
     push( @retval, "$ln" ) if ( -f "$dir$ln" );
   }
 
@@ -75,5 +103,12 @@ sub createFake {
 
 __DATA__
 ---
-'Bash Environment': '${WORKSPACE_DIR}/cxap/bashenvironment'
-'Vim Environment':  '${WORKSPACE_DIR}/cxap/vimenvironment'
+binaries:
+  'Bash Environment': '${WORKSPACE_DIR}/cxap/bashenvironment'
+  'Vim Environment':  '${WORKSPACE_DIR}/cxap/vimenvironment'
+libraries:
+  Toolkit:          '${WORKSPACE_DIR}/cxap/Toolkit/Toolkit'
+  tools:            '${WORKSPACE_DIR}/cxap/Toolkit/tools'
+  Utilities:        '${WORKSPACE_DIR}/cxap/Toolkit/Utilities'
+  PSInfo:           '${WORKSPACE_DIR}/cxap/Toolkit/PSInfo'
+  StructurePrinter: '${WORKSPACE_DIR}/cxap/Toolkit/StructurePrinter'
