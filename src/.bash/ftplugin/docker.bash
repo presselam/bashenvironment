95e379df21911a58721e78a9705ba125efb525bd
Andrew M. Pressel <ufotofu@whistlinglemons.com>
2022-08-09 10:33:40 -0400

    normalize dbuild and drun

--- src/.bash/ftplugin/docker.bash
+++ src/.bash/ftplugin/docker.bash
@@ -8,70 +8,75 @@ alias dlogin='ln -s ~/.docker/config.json.keep ~/.docker/config.json'
 alias dlogout='rm -f ~/.docker/config.json'
 
 function dbuild {
-  registry='harbor.nasiccloud.io/dms/'
+  args=$(getopt -o pns:v:c:r: --long plain,nocache,service:,version:,context:,registry: -n dbuild -a -- "$@")
+  valid=$?
+  if [[ "${valid}" != "0" ]]; then
+    return 1
+  fi
+
+  registry='local-registry'
   service=$(basename "$(pwd)")
   version='0.0.0'
-  context='..'
+  context='.'
   progress='auto'
   dfile="$(pwd)/Dockerfile"
   cache=
 
-  if [[ -f VERSION.txt ]];then
+  if [[ -f VERSION.txt ]]; then
     version=$(cat VERSION.txt)
   fi
 
-  for arg in "$@"
-  do  
-    case "${arg}" in
-    --progress=*)
-      progress=${arg#*=}
-    ;;
-    --plain)
-      progress='plain'
-    ;;
-    --cache)
-      cache=
-    ;;
-    --nocache)
-      cache='--no-cache'
-    ;;
-    --registry=*)
-      registry=${arg#*=}
-    ;;
-    --service=*)
-      service=${arg#*=}
-    ;;
-    --version=*)
-      version=${arg#*=}
-    ;;
-    --context=*)
-      context=${arg#*=}
-    ;;
+  eval set -- "${args}"
+  while :
+  do
+    case "$1" in
+      -p | --plain)    progress='plain';   shift   ;;
+      -n | --nocache)  cache='--no-cache'; shift   ;;
+      -s | --service)  service="$2";       shift 2 ;;
+      -v | --version)  version="$2";       shift 2 ;;
+      -c | --context)  context="$2";       shift 2 ;;
+      -r | --registry) registry="$2";      shift 2 ;;
+      --) shift; break;;
     esac
   done
 
-  docker build --build-arg PULL_REGISTRY="${registry}" ${cache} --progress "${progress}" --tag "local-dms/${service}:${version}-PRESSEL" --file "${dfile}" "${context}"
+  image="${registry}/${service}:${version}-PRESSEL"
 
-  message_alert "Built image: local-dms/${service}:${version}-PRESSEL"
+  # shellcheck disable=SC2086
+  # workdbuildargs needs to be expanded
+  docker build ${WORKDBUILDARGS} ${cache} --progress "${progress}" --tag "${image}" --file "${dfile}" "${context}"
+
+  message_alert "Built image: ${image}"
 }
 
 function drun {
-  registry='harbor.nasiccloud.io/dms/'
+  args=$(getopt -o s:v:r: --long service:,version:,registry: -n drun -a -- "$@")
+  valid=$?
+  if [[ "${valid}" != "0" ]]; then
+    return 1
+  fi
+
+  registry='local-registry'
   service=$(basename "$(pwd)")
   version='0.0.0'
-  context='..'
-  progress='auto'
-  dfile="$(pwd)/Dockerfile"
-  cache=
 
-  if [[ -f VERSION.txt ]];then
+  if [[ -f VERSION.txt ]]; then
     version=$(cat VERSION.txt)
   fi
 
-  declare entry
-  if [[ $# -gt 0 ]]; then
-    entry="--entrypoint $@"
-  fi
+  eval set -- "${args}"
+  while :
+  do
+    case "$1" in
+      -s | --service)  service="$2";  shift 2 ;;
+      -v | --version)  version="$2";  shift 2 ;;
+      -r | --registry) registry="$2"; shift 2 ;;
+      --) shift; break;;
+    esac
+  done
 
-  docker run -it ${entry} "local-dms/${service}:${version}-PRESSEL" 
+  image="${registry}/${service}:${version}-PRESSEL"
+  message_alert "$@"
+  docker run -it "${image}" "$@"
 }
+
